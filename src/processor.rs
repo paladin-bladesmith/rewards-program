080c2e2b05ee46bd570ea48e84f9c509aa6eb88a
Joe Caulfield <joe.caulfield@anza.xyz>
2024-06-03 21:25:19 -0500

    processor: initialize holder rewards pool

--- src/processor.rs
+++ src/processor.rs
@@ -1,18 +1,171 @@
 //! Program processor.
 
 use {
-    crate::instruction::PaladinRewardsInstruction,
-    solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, msg, pubkey::Pubkey},
-    spl_transfer_hook_interface::instruction::TransferHookInstruction,
+    crate::{
+        error::PaladinRewardsError,
+        extra_metas::get_extra_account_metas,
+        instruction::PaladinRewardsInstruction,
+        state::{
+            get_holder_rewards_pool_address_and_bump_seed, HolderRewardsPool,
+            SEED_PREFIX_HOLDER_REWARDS_POOL,
+        },
+    },
+    solana_program::{
+        account_info::{next_account_info, AccountInfo},
+        entrypoint::ProgramResult,
+        msg,
+        program::invoke_signed,
+        program_error::ProgramError,
+        program_option::COption,
+        pubkey::Pubkey,
+        system_instruction,
+    },
+    spl_tlv_account_resolution::state::ExtraAccountMetaList,
+    spl_token_2022::{
+        extension::{transfer_hook::TransferHook, BaseStateWithExtensions, StateWithExtensions},
+        state::Mint,
+    },
+    spl_transfer_hook_interface::{
+        collect_extra_account_metas_signer_seeds, get_extra_account_metas_address_and_bump_seed,
+        instruction::{ExecuteInstruction, TransferHookInstruction},
+    },
 };
 
 /// Processes an
 /// [InitializeHolderRewardsPool](enum.PaladinRewardsInstruction.html)
 /// instruction.
 fn process_initialize_holder_rewards_pool(
-    _program_id: &Pubkey,
-    _accounts: &[AccountInfo],
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
 ) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+
+    let holder_rewards_pool_info = next_account_info(accounts_iter)?;
+    let extra_metas_info = next_account_info(accounts_iter)?;
+    let mint_info = next_account_info(accounts_iter)?;
+    let mint_authority_info = next_account_info(accounts_iter)?;
+    let _system_program_info = next_account_info(accounts_iter)?;
+
+    // Run checks on the mint.
+    {
+        // Ensure the mint is owned by SPL Token-2022.
+        if !mint_info.owner.eq(&spl_token_2022::id()) {
+            return Err(ProgramError::InvalidAccountOwner);
+        }
+
+        let mint_data = mint_info.try_borrow_data()?;
+        let mint = StateWithExtensions::<Mint>::unpack(&mint_data)?;
+
+        // Ensure the mint is configured with the `TransferHook` extension,
+        // and the program ID is the Paladin Rewards program.
+        let transfer_hook = mint.get_extension::<TransferHook>()?;
+        let hook_program_id: Option<Pubkey> = transfer_hook.program_id.into();
+        if !hook_program_id.eq(&Some(*program_id)) {
+            return Err(PaladinRewardsError::IncorrectTransferHookProgramId.into());
+        }
+
+        // Ensure the provided mint authority is the correct mint authority.
+        if !mint
+            .base
+            .mint_authority
+            .eq(&COption::Some(*mint_authority_info.key))
+        {
+            return Err(PaladinRewardsError::IncorrectMintAuthority.into());
+        }
+
+        // Ensure the mint authority is a signer.
+        if !mint_authority_info.is_signer {
+            return Err(ProgramError::MissingRequiredSignature);
+        }
+    }
+
+    // Initialize the holder rewards pool account.
+    {
+        let (holder_rewards_pool_address, holder_rewards_pool_bump) =
+            get_holder_rewards_pool_address_and_bump_seed(mint_info.key);
+        let holder_rewards_pool_signer_seeds = &[
+            SEED_PREFIX_HOLDER_REWARDS_POOL,
+            mint_info.key.as_ref(),
+            &[holder_rewards_pool_bump],
+        ];
+
+        // Ensure the provided holder rewards pool address is the correct
+        // address derived from the mint.
+        if !holder_rewards_pool_info
+            .key
+            .eq(&holder_rewards_pool_address)
+        {
+            return Err(PaladinRewardsError::IncorrectHolderRewardsPoolAddress.into());
+        }
+
+        // Ensure the holder rewards pool account has not already been
+        // initialized.
+        if holder_rewards_pool_info.data_len() != 0 {
+            return Err(ProgramError::AccountAlreadyInitialized);
+        }
+
+        // Allocate & assign.
+        invoke_signed(
+            &system_instruction::allocate(
+                &holder_rewards_pool_address,
+                std::mem::size_of::<HolderRewardsPool>() as u64,
+            ),
+            &[holder_rewards_pool_info.clone()],
+            &[holder_rewards_pool_signer_seeds],
+        )?;
+        invoke_signed(
+            &system_instruction::assign(&holder_rewards_pool_address, program_id),
+            &[holder_rewards_pool_info.clone()],
+            &[holder_rewards_pool_signer_seeds],
+        )?;
+
+        // Write the data.
+        let holder_rewards_pool_state = HolderRewardsPool::default();
+        let holder_rewards_pool_data = bytemuck::bytes_of(&holder_rewards_pool_state);
+        holder_rewards_pool_info
+            .try_borrow_mut_data()?
+            .copy_from_slice(holder_rewards_pool_data);
+    }
+
+    // Initialize the extra metas account.
+    {
+        let (extra_metas_address, extra_metas_bump) =
+            get_extra_account_metas_address_and_bump_seed(mint_info.key, program_id);
+        let extra_metas_bump = [extra_metas_bump];
+        let extra_metas_signer_seeds =
+            collect_extra_account_metas_signer_seeds(mint_info.key, &extra_metas_bump);
+
+        // Ensure the provided extra metas address is the correct address
+        // derived from the mint.
+        if !extra_metas_info.key.eq(&extra_metas_address) {
+            return Err(PaladinRewardsError::IncorrectExtraMetasAddress.into());
+        }
+
+        // Ensure the extra metas account has not already been initialized.
+        if extra_metas_info.data_len() != 0 {
+            return Err(ProgramError::AccountAlreadyInitialized);
+        }
+
+        let extra_metas = get_extra_account_metas()?;
+        let account_size = ExtraAccountMetaList::size_of(extra_metas.len())?;
+
+        // Allocate & assign.
+        invoke_signed(
+            &system_instruction::allocate(&extra_metas_address, account_size as u64),
+            &[extra_metas_info.clone()],
+            &[&extra_metas_signer_seeds],
+        )?;
+        invoke_signed(
+            &system_instruction::assign(&extra_metas_address, program_id),
+            &[extra_metas_info.clone()],
+            &[&extra_metas_signer_seeds],
+        )?;
+
+        // Write the data.
+        let mut data = extra_metas_info.try_borrow_mut_data()?;
+        ExtraAccountMetaList::init::<ExecuteInstruction>(&mut data, &extra_metas)?;
+    }
+
     Ok(())
 }
 
