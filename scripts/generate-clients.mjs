0f26c6a807d1ff5cc68f5b36615a410ff118302a
Joe Caulfield <joe.caulfield@anza.xyz>
2024-06-14 11:01:18 -0500

    drop `counter` boilerplate

--- scripts/generate-clients.mjs
+++ scripts/generate-clients.mjs
@@ -17,51 +17,6 @@ kinobi.update(
   })
 );
 
-// Update accounts.
-kinobi.update(
-  k.updateAccountsVisitor({
-    counter: {
-      seeds: [
-        k.constantPdaSeedNodeFromString("utf8", "counter"),
-        k.variablePdaSeedNode(
-          "authority",
-          k.publicKeyTypeNode(),
-          "The authority of the counter account"
-        ),
-      ],
-    },
-  })
-);
-
-// Update instructions.
-kinobi.update(
-  k.updateInstructionsVisitor({
-    create: {
-      byteDeltas: [k.instructionByteDeltaNode(k.accountLinkNode("counter"))],
-      accounts: {
-        counter: { defaultValue: k.pdaValueNode("counter") },
-        payer: { defaultValue: k.accountValueNode("authority") },
-      },
-    },
-    increment: {
-      accounts: {
-        counter: { defaultValue: k.pdaValueNode("counter") },
-      },
-      arguments: {
-        amount: { defaultValue: k.noneValueNode() },
-      },
-    },
-  })
-);
-
-// Set account discriminators.
-const key = (name) => ({ field: "key", value: k.enumValueNode("Key", name) });
-kinobi.update(
-  k.setAccountDiscriminatorFromFieldVisitor({
-    counter: key("counter"),
-  })
-);
-
 // Render JavaScript.
 const jsClient = path.join(__dirname, "..", "clients", "js");
 kinobi.accept(
