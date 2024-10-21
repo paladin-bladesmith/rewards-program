6dcb12f12bbbf9b22d128e3fd66ea9acddfec98a
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2024-10-21 12:15:57 -0500

    address clippy

--- program/src/constants.rs
+++ program/src/constants.rs
@@ -1,6 +1,7 @@
 pub const RENT_DEBT_NUMERATOR: u64 = 11;
-pub const RENT_DEBT_DENOMINATOR: u64 = 11;
+pub const RENT_DEBT_DENOMINATOR: u64 = 10;
 
+#[allow(clippy::arithmetic_side_effects)]
 pub const fn rent_debt(rent_paid: u64) -> u64 {
     rent_paid * RENT_DEBT_NUMERATOR / RENT_DEBT_DENOMINATOR
 }
